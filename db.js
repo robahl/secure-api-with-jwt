aa4e399ff1bc14ecf4b42c5c1cc18fecf280a67e
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-01 22:35:21 +0200

    Setup db connection

---
+++ db.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+
+const user = process.env.DBUSER;
+const pass = process.env.DBPASS;
+
+const mongoUri = `mongodb+srv://${user}:${pass}@haven-qjdfk.mongodb.net/test?retryWrites=true`;
+
+
+module.exports = {
+  connect: () => {
+    mongoose.connect(mongoUri, {useNewUrlParser: true});
+    console.log("Connected to MongoDB");
+  }
+};
