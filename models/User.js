deaed36339155babae281e522017a9f34eb70af2
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-02 21:19:15 +0200

    Chang to User model

--- models/User.js
+++ models/User.js
@@ -3,9 +3,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  admin: Boolean
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  admin: {
+    type: Boolean,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
