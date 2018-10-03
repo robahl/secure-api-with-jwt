ebb1dc067d5bde65f6ab832dd96f26fd0fb3bf6f
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-03 21:08:46 +0200

    Add login handler route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const db = require('./db');
 const userRoute = require('./routes/user');
+const loginHandler = require('./routes/login');
 
 const port = 3300;
 
@@ -11,6 +12,7 @@ db.connect();
 app.use(bodyParser.json());
 
 app.use('/users', userRoute);
+app.use(loginHandler);
 
 app.get('/', (req, res) => {
   res.json({awake: true});
