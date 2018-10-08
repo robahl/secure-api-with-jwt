d5b8c93f98c4380bcebd7bf4931eb64badb96284
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-08 20:57:50 +0200

    Return a JSON web token on successful login

--- controllers/login.js
+++ controllers/login.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 router.post('/login', (req, res) => {
   if (req.body.username && req.body.password) {
     User.findOne({username: req.body.username})
       .then(user => {
         if (user.comparePassword(req.body.password)) {
-          res.json({success: "username and password matched"});
+          let payload = {
+            username: user.username
+          }
+
+          jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: 500}, jwt => {
+            res.json({
+              success: "username and password matched",
+              jwt
+            });
+          });
         } else {
           res.json({error: "Username and password mismatch"});
         }
