4785d81a8180f75aab18a9122d9a98f8fce053e5
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-10 22:22:45 +0200

    Add a done handle to let Jest know when async functions are complete

--- test/server.test.js
+++ test/server.test.js
@@ -2,10 +2,11 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('app is alive', () => {
-  test('"/" responds with awake message', () => {
+  test('"/" responds with awake message', (done) => {
     request(app).get('/').then(response => {
       expect(response.statusCode).toBe(200);
       expect(response.body.awake).toBeTruthy();
+      done();
     });
   });
 });
