a32bf33ba29ae94b1b478fde94d7e3990621c0c1
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-10 22:21:43 +0200

    Add user test

---
+++ test/user.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../app');
+const User = require('../models/User');
+const db = require('../db');
+
+const usersURL = '/users';
+
+afterAll((done) => {
+  db.disconnect();
+  done();
+});
+
+describe('User controller', () => {
+  test(`${usersURL} responds with an error without JWT`, (done) => {
+    request(app).get(`/users`).then(response => {
+      expect(response.statusCode).toBe(401);
+      done();
+    });
+  });
+});
