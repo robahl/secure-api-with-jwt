fe5ec417d032388cffedb2c19577b42f6c7a6f0c
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-21 23:48:51 +0200

    Prettier

--- test/server.test.js
+++ test/server.test.js
@@ -2,11 +2,13 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('app is alive', () => {
-  test('"/" responds with awake message', (done) => {
-    request(app).get('/').then(response => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body.awake).toBeTruthy();
-      done();
-    });
+  test('"/" responds with awake message', done => {
+    request(app)
+      .get('/')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body.awake).toBeTruthy();
+        done();
+      });
   });
 });
--- test/user.test.js
+++ test/user.test.js
@@ -5,33 +5,35 @@ const db = require('../db');
 
 const usersURL = '/users';
 
-afterAll((done) => {
+afterAll(done => {
   db.disconnect();
   done();
 });
 
 describe('User controller', () => {
-  test(`${usersURL} responds with an error without JWT`, (done) => {
-    request(app).get(usersURL).then(response => {
-      expect(response.statusCode).toBe(401);
-      done();
-    });
+  test(`${usersURL} responds with an error without JWT`, done => {
+    request(app)
+      .get(usersURL)
+      .then(response => {
+        expect(response.statusCode).toBe(401);
+        done();
+      });
   });
 
-  test(`${usersURL}/add creates a new user`, (done) => {
-    const testUser = {username: "test_user", password: "testpassword"};
+  test(`${usersURL}/add creates a new user`, done => {
+    const testUser = { username: 'test_user', password: 'testpassword' };
     request(app)
       .post(`${usersURL}/add`)
       .send(testUser)
       .then(response => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body.success).toBeTruthy();
-      User.findOne({username: testUser.username}).then(user => {
-        expect(user.username).toBe(testUser.username);
-        User.deleteOne({_id: user._id})
-          .then(() => done())
-          .catch(err => console.log(err));
+        expect(response.statusCode).toBe(200);
+        expect(response.body.success).toBeTruthy();
+        User.findOne({ username: testUser.username }).then(user => {
+          expect(user.username).toBe(testUser.username);
+          User.deleteOne({ _id: user._id })
+            .then(() => done())
+            .catch(err => console.log(err));
+        });
       });
-    });
   });
 });
